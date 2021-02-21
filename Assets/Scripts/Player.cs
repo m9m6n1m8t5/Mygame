c6fa837a1671ee017792b01f86f9ac98c3fb15a3
Naoto Toyama <m9m6n1m8t51080@gmail.com>
2021-02-21 21:44:24 +0900

    fire bullets

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,29 +5,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject bullet;
+
     Camera cam;
+    float speed;
+    float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.FindObjectOfType<Camera>();
         transform.eulerAngles = new Vector3(0f, 0f, 30f);
+        speed = 1f;
+        rotationSpeed = 60f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //移動
-        if (Input.GetKey(KeyCode.W)) transform.Translate(0f, 1f * Time.deltaTime, 0f);
-        if (Input.GetKey(KeyCode.S)) transform.Translate(0f, -1f * Time.deltaTime, 0f);
-        if (Input.GetKey(KeyCode.A)) transform.Translate(-1f * Time.deltaTime, 0f, 0f);
-        if (Input.GetKey(KeyCode.D)) transform.Translate(1f * Time.deltaTime, 0f, 0f);
-        //向き
         float angle_radian = (transform.eulerAngles.z + 90f) * Mathf.PI / 180;
         Vector2 forword = new Vector2(Mathf.Cos(angle_radian), Mathf.Sin(angle_radian));
         Vector2 toMouse = Input.mousePosition - cam.WorldToScreenPoint(transform.position);
         print($"{transform.eulerAngles}, {toMouse}, {forword}, {Vector2.SignedAngle(forword, toMouse)}");
-        transform.Rotate(new Vector3(0f, 0f, 30f * Time.deltaTime * Mathf.Sign(Vector2.SignedAngle(forword, toMouse))));
+
+        //移動
+        if (toMouse.magnitude > 30f) transform.Translate(0f, speed * Time.deltaTime, 0f);
+
+        //向き
+        transform.Rotate(new Vector3(0f, 0f, rotationSpeed * Time.deltaTime * Mathf.Sign(Vector2.SignedAngle(forword, toMouse))));
+
+        //弾の発射
+        if (Input.GetMouseButtonDown(0)) Instantiate(bullet, transform.position, transform.rotation);
+        
+        //キー入力
+        //if (Input.GetKey(KeyCode.W)) transform.Translate(0f, 1f * Time.deltaTime, 0f);
+        //if (Input.GetKey(KeyCode.S)) transform.Translate(0f, -1f * Time.deltaTime, 0f);
+        //if (Input.GetKey(KeyCode.A)) transform.Translate(-1f * Time.deltaTime, 0f, 0f);
+        //if (Input.GetKey(KeyCode.D)) transform.Translate(1f * Time.deltaTime, 0f, 0f);
+
 
 
     }
