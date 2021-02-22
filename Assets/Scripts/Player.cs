a412bee76f28a45f256f2c507b35198d6afb263a
Naoto Toyama <m9m6n1m8t51080@gmail.com>
2021-02-22 20:59:35 +0900

    fixed bullet firing position

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,16 +8,20 @@ public class Player : MonoBehaviour
     public GameObject bullet;
 
     Camera cam;
+    Transform image;
     float speed;
     float rotationSpeed;
+    float radius;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.FindObjectOfType<Camera>();
-        transform.eulerAngles = new Vector3(0f, 0f, 30f);
         speed = 1f;
         rotationSpeed = 60f;
+        image = transform.Find("player");
+        radius = image.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+
     }
 
     // Update is called once per frame
@@ -26,7 +30,7 @@ public class Player : MonoBehaviour
         float angle_radian = (transform.eulerAngles.z + 90f) * Mathf.PI / 180;
         Vector2 forword = new Vector2(Mathf.Cos(angle_radian), Mathf.Sin(angle_radian));
         Vector2 toMouse = Input.mousePosition - cam.WorldToScreenPoint(transform.position);
-        print($"{transform.eulerAngles}, {toMouse}, {forword}, {Vector2.SignedAngle(forword, toMouse)}");
+        // print($"{transform.eulerAngles}, {toMouse}, {forword}, {Vector2.SignedAngle(forword, toMouse)}");
 
         //移動
         if (toMouse.magnitude > 30f) transform.Translate(0f, speed * Time.deltaTime, 0f);
@@ -35,7 +39,7 @@ public class Player : MonoBehaviour
         transform.Rotate(new Vector3(0f, 0f, rotationSpeed * Time.deltaTime * Mathf.Sign(Vector2.SignedAngle(forword, toMouse))));
 
         //弾の発射
-        if (Input.GetMouseButtonDown(0)) Instantiate(bullet, transform.position, transform.rotation);
+        if (Input.GetMouseButtonDown(0)) Instantiate(bullet, transform.position + new Vector3(forword.x, forword.y, 0f) * radius, transform.rotation);
         
         //キー入力
         //if (Input.GetKey(KeyCode.W)) transform.Translate(0f, 1f * Time.deltaTime, 0f);
